60393e473941bff859f939dbe845296697382e05
KarthickWebDeveloper <karthicklearnings@gmail.com>
2025-03-26 19:25:55 +0530

    latest backend connected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+// import { ToastContainer } from "react-toastify";
 
 import Home from "./pages/Home";
 import PostJob from "./pages/PostJob";
--- client/src/redux/Store.jsx
+++ client/src/redux/Store.jsx
@@ -2,11 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./AuthSlice";
 import searchReducer from "./SearchSlice";
 
-const store = configureStore({
+const Store = configureStore({
   reducer: {
     auth: authReducer,
     search: searchReducer,
   },
 });
 
-export default store;
+export default Store;

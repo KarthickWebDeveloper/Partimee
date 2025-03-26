9df9aab8d2f376c3a6076c93f98bc4fd2aa0cc7b
KarthickWebDeveloper <karthicklearnings@gmail.com>
2025-03-26 20:10:19 +0530

    render backend connected

--- server/server.js
+++ server/server.js
@@ -20,4 +20,4 @@ app.use("/api/uploads", uploadRoutes);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port :${PORT}`));
